ed63f11db07f82ea61cb00bb02c1c503cc4ff77f
vytautasb <vytautas.buitvydas@gmail.com>
2017-05-25 15:24:49 +0300

    Ver.2.0 Update for new hardwate version HW_VER_v1.3

--- gen_prg_files.tcl
+++ gen_prg_files.tcl
@@ -1,10 +1,10 @@
 #Copy and Rename .sof file by hardware version 
-file copy -force -- output_files/LimeSDR-PCIE_lms7_trx.sof output_files/LimeSDR-PCIE_lms7_trx_HW_1.2.sof
+file copy -force -- output_files/LimeSDR-PCIE_lms7_trx.sof output_files/LimeSDR-PCIE_lms7_trx_HW_1.3.sof
 qexec "quartus_cpf -c output_files/jic_gen_setup.cof"
 post_message "*******************************************************************"
-post_message "Generated programming file: LimeSDR-PCIE_lms7_trx_HW_1.2.jic" -submsgs [list "Ouput file saved in /output_files directory"]
+post_message "Generated programming file: LimeSDR-PCIE_lms7_trx_HW_1.3.jic" -submsgs [list "Ouput file saved in /output_files directory"]
 post_message "*******************************************************************"
 qexec "quartus_cpf -c output_files/rbf_gen_setup.cof"
 post_message "*******************************************************************"
-post_message "Generated programming file: LimeSDR-PCIE_lms7_trx_HW_1.2.rbf" -submsgs [list "Ouput file saved in /output_files directory"]
+post_message "Generated programming file: LimeSDR-PCIE_lms7_trx_HW_1.3.rbf" -submsgs [list "Ouput file saved in /output_files directory"]
 post_message "*******************************************************************"
